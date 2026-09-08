ce16ce607094b2dbf3fd7997287ab5611b7e3bdc
RBN <iq.rubenop@gmail.com>
2026-09-08 15:36:48 -0600

    Still Day 1. I am getting into the rythm.

--- Assessments/Day001/AssessmenDay001/AssessmenDay001/Assessment001_Review.md
+++ Assessments/Day001/AssessmenDay001/AssessmenDay001/Assessment001_Review.md
@@ -1,4 +1,8 @@
-﻿# Day 1 Assessment Review
+﻿
+
+
+
+# Day 1 Assessment Review
 
 Date: September 7, 2026
 
@@ -12,6 +16,9 @@ I used a bit of
 - Google
 - Documentation
 to verify the syntax of some statements.
+
+
+
 
 ## Problem 1 - Character frequency
 
@@ -36,3 +43,51 @@ I need to start using pseudocode before writing the actual program
 to have a better understanding of what needs to be done.
 
 
+
+
+## Problem 2 -  Second largest distinct number
+
+### Did I solve it independently?
+
+Yes
+
+### What gave me trouble?
+I didn't know with what values I should be initializing the two 
+variables that are needed to solve this problem.
+
+Also, I still hesitated when wrote the conditionals to swap values between
+largest and second different largest.
+
+Finally, I also found trouble with arrays with values where there is not a 
+second largest value, meaning that all values are equal. I also didn't know 
+how to decide what message should be printed when this case was the current case.
+
+### What did I have to look up?
+I had intellisense activated and it helped me to solve the problem. 
+I didn't really use any other source.
+
+
+### What would I do differently?
+Again, I think pseudocode would help me a lot.
+
+
+
+
+## Problem 3 - 
+
+### Did I solve it independently?
+
+Yes
+
+### What gave me trouble?
+What gave me trouble here was that I didn't use top level statements,
+so the limits of the main entry are not clear and I thought classes 
+could be written at the top of the program, but I found it wasn't that way.
+
+
+### What did I have to look up?
+Nothing
+
+
+### What would I do differently?
+I think I need to improve the writing speed.
--- Assessments/Day001/AssessmenDay001/AssessmenDay001/Program.cs
+++ Assessments/Day001/AssessmenDay001/AssessmenDay001/Program.cs
@@ -1,32 +1,32 @@
 ﻿//*********************************************************************************************************
 //Problem 1 — Character frequency
 
-string text = "Programming!$%&";
+//string text = "Programming!$%&";
 
-CountCharacters(text);
+//CountCharacters(text);
 
-foreach (var kvp in CountCharacters(text))
-{
-    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
-}
+//foreach (var kvp in CountCharacters(text))
+//{
+//    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+//}
 
 
-Dictionary<char, int> CountCharacters(string text)
-{
-    Dictionary<char, int> charCounter = new Dictionary<char, int>();
-    foreach (char c in text)
-    {
-        if (charCounter.ContainsKey(c))
-        {
-            charCounter[c]++;
-        }
-        else
-        {
-            charCounter[c] = 1;
-        }
-    }
-    return charCounter;
-}
+//Dictionary<char, int> CountCharacters(string text)
+//{
+//    Dictionary<char, int> charCounter = new Dictionary<char, int>();
+//    foreach (char c in text)
+//    {
+//        if (charCounter.ContainsKey(c))
+//        {
+//            charCounter[c]++;
+//        }
+//        else
+//        {
+//            charCounter[c] = 1;
+//        }
+//    }
+//    return charCounter;
+//}
 
 
 //*********************************************************************************************************
@@ -37,22 +37,26 @@ Dictionary<char, int> CountCharacters(string text)
 //int largest = int.MinValue;
 //int secondLargest = int.MinValue;
 
-//for(int i=0;i<numbers.Length;i++){
-//    if(numbers[i]>largest){
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] > largest)
+//    {
 //        secondLargest = largest;
 //        largest = numbers[i];
 //    }
-//    else if(numbers[i]>secondLargest && numbers[i] != largest)
+//    else if (numbers[i] > secondLargest && numbers[i] != largest)
 //    {
 //        secondLargest = numbers[i];
 //    }
 
 //}
 
-//if(secondLargest==int.MinValue){
+//if (secondLargest == int.MinValue)
+//{
 //    Console.WriteLine($"There is no second largest distinct number.");
 //}
-//else{
+//else
+//{
 //    Console.WriteLine($"Second Largest: {secondLargest}");
 //}
 
@@ -64,7 +68,7 @@ Dictionary<char, int> CountCharacters(string text)
 
 //Employee se1 = new SalariedEmployee("Ruben", "001", 80000);
 //Employee se2 = new SalariedEmployee("Luisa", "002", 90000);
-//Employee he1 = new HourlyEmployee("Beto", "003", 8,100);
+//Employee he1 = new HourlyEmployee("Beto", "003", 8, 100);
 //Employee he2 = new HourlyEmployee("Lula", "004", 8, 80);
 
 //employees.Add(se1);
@@ -72,29 +76,35 @@ Dictionary<char, int> CountCharacters(string text)
 //employees.Add(he1);
 //employees.Add(he2);
 
-//foreach(Employee e in employees){
+//foreach (Employee e in employees)
+//{
 //    Console.WriteLine($"Name: {e.Name}   Salary: {e.CalculatePayment()}");
 //}
 
-//public abstract class Employee{
+//public abstract class Employee
+//{
 //    public string? Name { get; protected set; }
 //    public string? Id { get; protected set; }
-//    public Employee(string name=null, string id=null){
+//    public Employee(string name = null, string id = null)
+//    {
 //        Name = name;
 //        Id = id;
 //    }
 //    public abstract decimal CalculatePayment();
 
 //}
-//public class SalariedEmployee : Employee{
+//public class SalariedEmployee : Employee
+//{
 //    public decimal MonthlySalary { get; private set; }
 
-//    public void SetMonthlySalary(decimal monthlySalary){
-//        if (monthlySalary > 0){
-//            MonthlySalary= monthlySalary;
+//    public void SetMonthlySalary(decimal monthlySalary)
+//    {
+//        if (monthlySalary > 0)
+//        {
+//            MonthlySalary = monthlySalary;
 //        }
 //    }
-//    public SalariedEmployee(string name, string id, decimal monthlySalary):base(name,id)
+//    public SalariedEmployee(string name, string id, decimal monthlySalary) : base(name, id)
 //    {
 //        SetMonthlySalary(monthlySalary);
 //    }
@@ -116,13 +126,16 @@ Dictionary<char, int> CountCharacters(string text)
 //            HoursWorked = hoursWorked;
 //        }
 //    }
-//    public void SetHourlyRate(decimal hourlyRate){
-//        if(hourlyRate>0){
+//    public void SetHourlyRate(decimal hourlyRate)
+//    {
+//        if (hourlyRate > 0)
+//        {
 //            HourlyRate = hourlyRate;
 //        }
 //    }
 
-//    public HourlyEmployee (string name, string id, decimal hoursWorked, decimal hourlyRate ):base(name,id){
+//    public HourlyEmployee(string name, string id, decimal hoursWorked, decimal hourlyRate) : base(name, id)
+//    {
 //        SetHoursWorked(hoursWorked);
 //        SetHourlyRate(hourlyRate);
 //    }
